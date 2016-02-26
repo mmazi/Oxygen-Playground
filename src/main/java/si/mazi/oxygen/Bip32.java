da7a91dda9c43cb30f0245c245785875fde344ba
Matija Mazi <matija.mazi@gmail.com>
2016-02-26 09:39:00 +0100

    Recreate the same key hierarchy as in node.js

--- src/main/java/si/mazi/oxygen/Bip32.java
+++ src/main/java/si/mazi/oxygen/Bip32.java
@@ -1,35 +1,75 @@
 package si.mazi.oxygen;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
 import org.bitcoinj.crypto.ChildNumber;
+import org.bitcoinj.crypto.DeterministicHierarchy;
 import org.bitcoinj.crypto.DeterministicKey;
 import org.bitcoinj.crypto.HDKeyDerivation;
 import org.bitcoinj.params.MainNetParams;
+import org.ethereum.crypto.ECKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongycastle.util.encoders.Hex;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class Bip32 {
 
     private static final Logger log = LoggerFactory.getLogger(Bip32.class);
 
-    public static final String MASTER =
-            "xprv9s21ZrQH143K2f2DCtydaTP9iE1uZF4bFK7L8m6Lrceqi8Ejo1kBrBNUaQYgtHR9QD36CESdDyRSkMrmtriET94kR3auY64K8S4Kigcnzsz";
-
-    public static final MainNetParams MAINNET = MainNetParams.get();
+    private static final MainNetParams MAINNET = MainNetParams.get();
 
     public static void main(String[] args) {
-        DeterministicKey m = DeterministicKey.deserializeB58(MASTER, MAINNET);
+        DeterministicKey m = HDKeyDerivation.createMasterPrivateKey(Hex.decode("e16e395b7b6c8914903a216d09e470440ee685338a6ae784dd4890c7a184ee57"));
 
-        // derive m/44'/60'/0'/0
-        DeterministicKey userDepositsExtKey = HDKeyDerivation.deriveChildKey(m.derive(44).derive(60).derive(0), ChildNumber.ZERO);
+        writeSeed("Master", m);
 
-        log.info("userDepositsExtKey = {}", userDepositsExtKey.serializePubB58(MAINNET));
+        writeSeedDerived("User deposits", m, 0);
 
-        for (Integer i : Arrays.asList(100000000, 123456789, 999999999)) {
-            DeterministicKey userKey = HDKeyDerivation.deriveChildKey(userDepositsExtKey, new ChildNumber(i));
-            log.info("{}", new String(Hex.encode(userKey.getPrivKeyBytes())));
-        }
+        writeAccountAddress("Hot wallet - user deposits forward address", m, 1, 0);
+        writeAccountAddress("Hot wallet - refills from cold wallet", m, 1, 1);
+        writeAccountAddress("Cold wallet", m, 2, 0);
+    }
+
+    private static void writeSeed(String keyDesc, DeterministicKey key) {
+        log.info(keyDesc + ":");
+        log.info("    private: " + key.serializePrivB58(MAINNET));
+        log.info("     public: " + key.serializePubB58(MAINNET));
+    }
+
+    private static void writeSeedDerived(String keyDesc, DeterministicKey m, int account) {
+        writeSeed(keyDesc, deriveAccountExt(m, account));
+    }
+
+    private static void writeAccountAddress(String keyDesc, DeterministicKey m, int account, int subIdx) {
+        DeterministicKey key = deriveAccountExtSub(m, account, subIdx);
+        log.info(keyDesc + ":");
+        log.info("        key: " + key.getPrivateKeyAsHex());
+        log.info("    address: " + getEthereumAddress(key));
+    }
+
+    private static DeterministicKey deriveAccountExtSub(DeterministicKey m, int account, int addressIdx) {
+        //  m/44'/60'/account'/0/addressIdx;
+        List<ChildNumber> path =
+                ImmutableList.of(new ChildNumber(44, true), new ChildNumber(60, true), new ChildNumber(account, true), new ChildNumber(0, false),
+                                 new ChildNumber(addressIdx, false));
+        return derive(m, path);
+    }
+
+    private static DeterministicKey deriveAccountExt(DeterministicKey m, int account) {
+        //  m/44'/60'/account'/0
+        List<ChildNumber> path =
+                ImmutableList.of(new ChildNumber(44, true), new ChildNumber(60, true), new ChildNumber(account, true), new ChildNumber(0, false));
+        return derive(m, path);
+    }
+
+    private static DeterministicKey derive(DeterministicKey m, List<ChildNumber> path) {
+        log.debug("Path: m/{}", Joiner.on("/").join(path).replace('H', '\''));
+        return new DeterministicHierarchy(m).get(path, false, true);
+    }
+
+    private static String getEthereumAddress(DeterministicKey key) {
+        return "0x" + Hex.toHexString(new ECKey(null, key.getPubKeyPoint()).getAddress());
     }
 }
